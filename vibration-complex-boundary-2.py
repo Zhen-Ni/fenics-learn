b7020fa511af0999cc279050ca94afa0afb9ce54
Zhen NI <48048142+Zhen-Ni@users.noreply.github.com>
2019-08-01 18:16:13 +0800

    Update vibration-complex-boundary-2.py

--- vibration-complex-boundary-2.py
+++ vibration-complex-boundary-2.py
@@ -4,8 +4,7 @@ import numpy as np
 from fenics import *
 from ufl import nabla_div
 
-# Add spring in the final matrix directly
-
+# Add spring in the stiffness matrix directly
 
 E = 210e9
 rho = 7800
@@ -13,11 +12,11 @@ mu = 0.3
 W = 0.2
 L = 3.0
 g=9.8
-k=Constant(1e8)
+k=1e8
 
 
 # Create mesh and define function space
-mesh = BoxMesh(Point(0, 0, 0), Point(L, W, W), 60, 4, 4)
+mesh = BoxMesh(Point(0, 0, 0), Point(L, W, W), 15, 1, 1)
 V = VectorFunctionSpace(mesh, 'P', 3)
 
 def strain_normal(u):
@@ -46,16 +45,10 @@ u = TrialFunction(V)
 v = TestFunction(V)
 
 
-f = Constant((0, 0, -rho*g))
-T = Constant((0, 0, 0))
 
-
-delta = Delta(eps=5E-2, x0=(L,W,W),degree=5)
-delta2 = Delta(eps=5E-2, x0=(L,0,W),degree=5)
-
-a = (np.dot(stress_normal(u,E,mu), strain_normal(v)) + 
+ak = (np.dot(stress_normal(u,E,mu), strain_normal(v)) + 
      np.dot(stress_shear(u,E,mu), strain_shear(v)))*dx
-L = dot(f, v)*dx + dot(T, v)*ds
+am = rho * inner(u,v) * dx
 
 tol = 1E-14
 def clamped_boundary(x, on_boundary):
@@ -63,30 +56,54 @@ def clamped_boundary(x, on_boundary):
 
 bc = DirichletBC(V, Constant((0, 0, 0)), clamped_boundary)
 
+K = PETScMatrix()
+M = PETScMatrix()
 
-# Compute solution
+# Lead to asymmetric matrix
+#assemble(ak,tensor=K)
+#assemble(am,tensor=M)
+#bc.apply(K)
+#bc.apply(M)
+
+dummy = v[0]*dx
+assemble_system(ak,dummy,bc, A_tensor=K)
+assemble_system(am, dummy,bc ,A_tensor=M)
+bc.zero(M)
+
+dof_spring = []
+for i,(x,y,z) in enumerate(V.tabulate_dof_coordinates()):
+    if near(x,L) and near(y,W) and near(z,W):
+        dof_spring.append(i)
+    if near(x,L) and near(y,0) and near(z,W):
+        dof_spring.append(i)
+
+K.set(np.array([[k,0],[0,k]]), dof_spring,dof_spring)
+
+
+eigensolver = SLEPcEigenSolver(K,M)
+eigensolver.parameters["spectrum"] = "target magnitude"
+eigensolver.parameters["spectral_transform"] = "shift-and-invert"
+eigensolver.parameters["spectral_shift"] = 0.0
+eigensolver.solve(60)
+
+
+
+neigs = 50
+computed_eigenvalues = []
+for i in range(neigs):
+    r, _ = eigensolver.get_eigenvalue(i) # ignore the imaginary part
+    computed_eigenvalues.append(r**.5/2/pi)
+print(np.sort(np.array(computed_eigenvalues)))
+
+
+xdmffile = XDMFFile('vibration1d-complex-boundary-2.xdmf')
 u = Function(V)
-solve(a == L, u, bc, solver_parameters={'linear_solver':'mumps'})
-
-
-
-V = FunctionSpace(mesh, 'P', 1)
-
-# Compute magnitude of displacement
-u_magnitude = sqrt(dot(u, u))
-u_magnitude = project(u_magnitude, V)
-# plot(u_magnitude, 'Displacement magnitude')
-print('min/max u:',
-      u_magnitude.vector().min(),
-      u_magnitude.vector().max())
-
-# Save solution to file in VTK format
-u.rename('u','uu')
-File('displacement1d-complex-boundary-2.pvd') << u
-File('magnitude1d-complex-boundary-2.pvd') << u_magnitude
-
-## Hold plot
-#interactive()
+u.rename('u','uuuuu')
+for i in range(20):
+    r, c, rx, cx = eigensolver.get_eigenpair(i)
+    u.vector()[:] = rx
+    xdmffile.write(u, r**.5/2/pi)
+xdmffile.close()
 
 
 
